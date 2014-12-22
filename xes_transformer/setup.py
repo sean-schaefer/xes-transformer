b42db6d4ae3c20a9813b3d5ffbee4a9780cd341f
Sean Schaefer <sean.schaefer@pearson.com>
2014-12-22 11:12:54 -0700

    Install so Travis can find imports

---
+++ xes_transformer/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='xes_transformer',
+    version='0.0.1',
+    author='Sean Schaefer',
+    author_email='sean.schaefer2@gmail.com',
+    description='Transforms event logs into XES format'
+)
