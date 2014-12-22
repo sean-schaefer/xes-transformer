4bd61955361162b8112b3d60c436f874fa24e18c
Sean Schaefer <sean.schaefer@pearson.com>
2014-12-22 11:26:59 -0700

    Install so Travis can find imports

--- xes_transformer/tests/test_xes_transformer.py
+++
@@ -1,8 +0,0 @@
-import unittest
-from xes_transformer.transformer import xes_transformer
-
-
-class TestXesTransformer(unittest.TestCase):
-    def test_first_method(self):
-        print "Do you know where your towel is?"
-        self.assertTrue(xes_transformer.first_method())
--- xes_transformer/transformer/xes_transformer.py
+++ xes_transformer/transformer/xes_transformer.py
@@ -1,2 +1,15 @@
+import unittest
+
+
 def first_method():
     return True
+
+
+class TestXesTransformer(unittest.TestCase):
+    def test_first_method(self):
+        print "Do you know where your towel is?"
+        self.assertTrue(first_method())
+
+
+if __name__ == '__main__':
+    unittest.main()
